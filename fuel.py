88a9be920c1af64d1aa697f8256b6ea5b7a87e2d
oswald retard <oswald@420blaze.it>
2019-12-02 00:13:02 +0100

    ez clap

--- fuel.py
+++ fuel.py
@@ -1,7 +1,24 @@
 f = open("fuel_input.txt", "r")
+
+
+def fuel_mass(x):
+    return (int(int(x) / 3) - 2)
+
+
+def mass_fuel_rec(mass):
+    result = fuel_mass(mass)
+    if fuel_mass(result) > 0:
+        result += mass_fuel_rec(result)
+    return result
+
+
+# A1
 if f.mode == 'r':
     f1 = f.readlines()
-    fuel = 0
+    fuel_a1 = 0
+    fuel_a2 = 0
     for x in f1:
-        fuel += (int(int(x) / 3) - 2)
-print(fuel)
+        fuel_a1 += fuel_mass(x)
+        fuel_a2 += mass_fuel_rec(x)
+print(fuel_a1)
+print(fuel_a2)
