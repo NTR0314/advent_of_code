7e63a31b580dd41cb8f82ffc89d1f6af6a729164
oswald retard <oswald@420blaze.it>
2019-12-03 22:57:20 +0100

    robin der gott

--- day3/crossed_wires.py
+++ day3/crossed_wires.py
@@ -20,7 +20,11 @@ if f.mode == 'r':
     path2 = path2.split(",")
 
 path1_points = [(0, 0)]
+path1_dist_traveled = []
+path1_count = 1
 path2_points = [(0, 0)]
+path2_dist_traveled = []
+path2_count = 1
 
 for instr in path1:
     if instr[0] == 'R':
@@ -29,24 +33,32 @@ for instr in path1:
         for x in range(diff):
             y = x + 1
             path1_points.append((last_point[0] + y, last_point[1]))
+            path1_dist_traveled.append(path1_count)
+            path1_count += 1
     elif instr[0] == 'L':
         diff = abs(getInt(instr))
         last_point = (path1_points[-1][0], path1_points[-1][1])
         for x in range(diff):
             y = x + 1
             path1_points.append((last_point[0] - y, last_point[1]))
+            path1_dist_traveled.append(path1_count)
+            path1_count += 1
     elif instr[0] == 'U':
         diff = abs(getInt(instr))
         last_point = (path1_points[-1][0], path1_points[-1][1])
         for x in range(diff):
             y = x + 1
             path1_points.append((last_point[0], last_point[1] + y))
+            path1_dist_traveled.append(path1_count)
+            path1_count += 1
     elif instr[0] == 'D':
         diff = abs(getInt(instr))
         last_point = (path1_points[-1][0], path1_points[-1][1])
         for x in range(diff):
             y = x + 1
             path1_points.append((last_point[0], last_point[1] - y))
+            path1_dist_traveled.append(path1_count)
+            path1_count += 1
 
 for instr in path2:
     if instr[0] == 'R':
@@ -55,24 +67,32 @@ for instr in path2:
         for x in range(diff):
             y = x + 1
             path2_points.append((last_point[0] + y, last_point[1]))
+            path2_dist_traveled.append(path2_count)
+            path2_count += 1
     elif instr[0] == 'L':
         diff = abs(getInt(instr))
         last_point = (path2_points[-1][0], path2_points[-1][1])
         for x in range(diff):
             y = x + 1
             path2_points.append((last_point[0] - y, last_point[1]))
+            path2_dist_traveled.append(path2_count)
+            path2_count += 1
     elif instr[0] == 'U':
         diff = abs(getInt(instr))
         last_point = (path2_points[-1][0], path2_points[-1][1])
         for x in range(diff):
             y = x + 1
             path2_points.append((last_point[0], last_point[1] + y))
+            path2_dist_traveled.append(path2_count)
+            path2_count += 1
     elif instr[0] == 'D':
         diff = abs(getInt(instr))
         last_point = (path2_points[-1][0], path2_points[-1][1])
         for x in range(diff):
             y = x + 1
             path2_points.append((last_point[0], last_point[1] - y))
+            path2_dist_traveled.append(path2_count)
+            path2_count += 1
 
 path2_points = path2_points[1:]
 path1_points = path1_points[1:]
@@ -88,4 +108,14 @@ for tuple in duplicates:
         min = dist
 
 print(min)
-print("nigger")
+
+min = sys.maxsize
+
+for tuple in duplicates:
+    idx1 = path1_points.index(tuple)
+    idx2 = path2_points.index(tuple)
+    steps = path1_dist_traveled[idx1] + path2_dist_traveled[idx2]
+    if ( steps < min):
+        min = steps
+
+print(min)
