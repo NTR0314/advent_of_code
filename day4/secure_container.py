e65abb4722dc70a913abc5d1f02808f73a21d494
oswald <oswald@420blaze.it>
2019-12-06 01:14:15 +0100

    unscheon -2 punkte

--- day4/secure_container.py
+++ day4/secure_container.py
@@ -1,13 +1,26 @@
-count = 0
-
 def check_double(strn):
     result = False
     for i in range(len(strn)):
         if i == 0:
             continue
-        if (strn[i - 1] == strn[i]):
+        if strn[i - 1] == strn[i]:
             result = True
     return result
+
+
+def check_double2(strn):
+    result = False
+    bigger_group_char = 'x'
+    for i in range(len(strn) - 2):
+        if strn[i] == strn[i + 1]:
+            if strn[i + 1] != strn[i + 2] and not strn[i] == bigger_group_char:
+                result = True
+            bigger_group_char = strn[i]
+    if strn[-1] == strn[-2] != strn[-3]:
+        result = True
+
+    return result
+
 
 def check_not_decreasing(strn):
     result = True
@@ -18,10 +31,29 @@ def check_not_decreasing(strn):
             result = False
     return result
 
-for i in range(108457,562041):
-    strn = str(i)
-    a = check_double(strn)
-    b = check_not_decreasing(strn)
-    if (a and b):
-        count += 1
-print(count)
+
+def a1():
+    count = 0
+    for i in range(108457, 562041):
+        strn = str(i)
+        a = check_double(strn)
+        b = check_not_decreasing(strn)
+        if a and b:
+            count += 1
+    print(count)
+
+
+def a2():
+    count = 0
+    for i in range(108457, 562041):
+        strn = str(i)
+        a = check_not_decreasing(strn)
+        b = check_double2(strn)
+        if a and b:
+            count += 1
+            #  print(i)
+    print(count)
+
+
+a1()
+a2()
